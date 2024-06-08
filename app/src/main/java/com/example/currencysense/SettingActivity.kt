b0183422ea4452988f10bfccb64f4183e1a7c592
rag <riar8601@gmail.com>
2024-06-08 12:36:38 +0800

    Add Setting Activity

---
+++ app/src/main/java/com/example/currencysense/SettingActivity.kt
@@ -0,0 +1,19 @@
+package com.example.currencysense
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.currencysense.utils.CustomButton
+
+class SettingActivity : AppCompatActivity() {
+
+    private lateinit var btnHistory: CustomButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+        supportActionBar?.title = "Setting"
+
+        btnHistory.setButtonText("History")
+    }
+}
