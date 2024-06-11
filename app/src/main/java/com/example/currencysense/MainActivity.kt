1183495ca7924c80b49ad704d6e7c7dc4b37695e
rag <riar8601@gmail.com>
2024-06-11 13:29:46 +0800

    Add History Button Activity

--- app/src/main/java/com/example/currencysense/MainActivity.kt
+++ app/src/main/java/com/example/currencysense/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.currencysense
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.currencysense.CameraActivity
-import com.example.currencysense.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,9 +28,11 @@ class MainActivity : AppCompatActivity() {
             // Start CameraActivity when scan_button is clicked
             startActivity(Intent(this@MainActivity, CameraActivity::class.java))
         }
-    }
 
-    private fun enableEdgeToEdge() {
-        // Your implementation of edge-to-edge behavior, if needed
+        // Go to History Activity
+        findViewById<Button>(R.id.history_button).setOnClickListener {
+            startActivity(Intent(this@MainActivity, HistoryActivity::class.java))
+        }
+
     }
 }
