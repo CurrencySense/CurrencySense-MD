8d87a0acfb6384d1663bfff0137f64a3d35d474b
Devin-senpai~~~ <devinalfanius950@gmail.com>
2024-06-15 02:22:44 +0800

    Revamped the UI and Added Speech Synthesis for scanned bills on ResultActivity (Devin Alfanius)

--- app/src/main/java/com/example/currencysense/ResultActivity.kt
+++ app/src/main/java/com/example/currencysense/ResultActivity.kt
@@ -1,5 +1,6 @@
 package com.example.currencysense
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
@@ -7,15 +8,21 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import java.text.DecimalFormat
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        mediaPlayer = MediaPlayer()
+
         val imageView: ImageView = findViewById(R.id.imageView)
         val amountTextView: TextView = findViewById(R.id.amountTextView)
+        val prefixTextView: TextView = findViewById(R.id.prefixTextView)
 
         val imageUri = intent.getStringExtra("IMAGE_URI")
         val recognizedAmount = intent.getIntExtra("RECOGNIZED_AMOUNT", -1)
@@ -30,9 +37,38 @@ class ResultActivity : AppCompatActivity() {
         }
 
         if (recognizedAmount != -1) {
-            amountTextView.text = "Recognized Amount: Rp. $recognizedAmount"
+            val formattedAmount = formatCurrency(recognizedAmount)
+            prefixTextView.text = "Jumlah Uang Anda adalah:"
+            amountTextView.text = formattedAmount
+            // Play corresponding audio based on recognized amount
+            when (recognizedAmount) {
+                50000 -> playAudioFromRaw(R.raw.lima_puluh_ribu_audio)
+                10000 -> playAudioFromRaw(R.raw.sepuluh_ribu_audio)
+                5000 -> playAudioFromRaw(R.raw.lima_ribu_audio)
+                else -> Toast.makeText(this, "Unrecognized amount", Toast.LENGTH_SHORT).show()
+            }
         } else {
             Toast.makeText(this, "No recognized amount provided", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun formatCurrency(amount: Int): String {
+        val formatter = DecimalFormat("#,###")
+        return "Rp. ${formatter.format(amount)}"
+    }
+
+    private fun playAudioFromRaw(rawResourceId: Int) {
+        try {
+            mediaPlayer = MediaPlayer.create(this, rawResourceId)
+            mediaPlayer.start()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error playing audio", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release() // Release MediaPlayer resources
     }
 }
