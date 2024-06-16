6b044acff5d21cdfbf6780d892892b32e54fa4d8
rag <riar8601@gmail.com>
2024-06-16 16:15:58 +0800

    Fixed Customize Button

--- app/src/main/java/com/example/currencysense/utils/CustomButton.kt
+++ app/src/main/java/com/example/currencysense/utils/CustomButton.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
 import com.example.currencysense.R
 
-class CustomButton : AppCompatButton {
+class HistoryButton : AppCompatButton {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
@@ -19,6 +19,31 @@ class CustomButton : AppCompatButton {
     init {
         txtColor = ContextCompat.getColor(context, android.R.color.white)
         enabledBackground = ContextCompat.getDrawable(context, R.drawable.history_button) as Drawable
+    }
+
+    fun setButtonText(text: String) {
+        this.text = text
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = enabledBackground
+        setTextColor(txtColor)
+        textSize = 24f
+        gravity = Gravity.CENTER
+    }
+}
+
+class ScanButton : AppCompatButton {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    private var txtColor: Int = 0
+    private var enabledBackground: Drawable
+
+    init {
+        txtColor = ContextCompat.getColor(context, android.R.color.white)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.scan_button) as Drawable
     }
 
     fun setButtonText(text: String) {
